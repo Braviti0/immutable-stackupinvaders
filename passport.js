9876a7921fad51be3ce422f3873c77b537e015d7
agyeman nana <onekingtee@gmail.com>
2023-12-16 07:21:29 +0000

    initial commit

--- passport.js
+++ passport.js
@@ -1,11 +1,11 @@
 window.passport = new window.immutable.passport.Passport({
-    baseConfig: new window.immutable.config.ImmutableConfiguration({
-      environment: window.immutable.config.Environment.SANDBOX,
-    }),
-    clientId: 'enter-client-id',
-    redirectUri: 'redirect-url',
-    logoutRedirectUri: 'logout-url',
-    audience: 'platform_api',
-    scope: 'openid offline_access email transact'
-  });
+  baseConfig: new window.immutable.config.ImmutableConfiguration({
+    environment: window.immutable.config.Environment.SANDBOX,
+  }),
+  clientId: "Ij5KyvLZBTMxLvvYZNPkFIj2zlO8fGks",
+  redirectUri: "https://included-grossly-rodent.ngrok-free.app",
+  logoutRedirectUri: "https://included-grossly-rodent.ngrok-free.app/logout.html",
+  audience: "platform_api",
+  scope: "openid offline_access email transact",
+});
 
